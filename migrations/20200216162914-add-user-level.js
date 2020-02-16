16394e710b091f08c6d18984d0f316d00bed6774
alex-phillips <ahp118@gmail.com>
2020-02-16 12:08:06 -0500

    fixed typo

--- migrations/20200216162914-add-user-level.js
+++ migrations/20200216162914-add-user-level.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('uses', 'level', {
+    await queryInterface.addColumn('users', 'level', {
       type: Sequelize.TINYINT,
       after: 'settings',
       defaultValue: 1
