0aef070dd4135d9082b7313c611c6058a0fd5d38
Kode <kodestar@gmail.com>
2020-02-21 21:17:46 +0000

    list users

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,18 +1,9 @@
 <template>
   <div class="list-user" :class="{ edit: this.edit }">
-    <div class="list-item-inner">
-      <div class="list-item-front">
-
-        <div class="icon"><img class="app-icon" :src="'http://heimdallmaster.test/storage/' + this.icon"></div>
-        <div class="name">{{ this.user.username }}<span class="tags">dashboard, dev, work</span></div>
-        <div class="actions">
-          <q-btn size="12px" unelevated color="primary" @click="showBack()">Edit</q-btn>
-          <q-btn size="12px" unelevated color="grey-2" text-color="black">Delete</q-btn>
-        </div>
-      </div>
-      <div class="list-item-back">
-        <span class="close" @click="hideBack()">Close</span>
-      </div>
+    <div class="icon"><img class="app-icon" :src="'http://heimdallmaster.test/storage/' + this.icon"></div>
+    <div class="details"><div class="name">{{ this.user.username }}<span class="email">{{ this.user.email }}</span></div></div>
+    <div class="actions">
+      <q-btn size="12px" unelevated icon="more_vert" @click="showBack()"></q-btn>
     </div>
   </div>
 </template>
@@ -47,16 +38,33 @@ export default {
 }
 </script>
 <style lang="scss">
-.list-items {
-    .list-item {
-        .name {
-            width: 100%;
-            max-width: 300px;
-            padding: 0 15px 0 0;
-            display: flex;
-            flex-direction: column;
+
+    .list-user {
+      width: 100%;
+      max-width: 300px;
+      margin: 20px;
+      display: flex;
+      background: #fafafa;
+      padding: 20px;
+      .icon {
+        margin-right: 20px;
+        img {
+          max-height: 75px;
         }
+      }
+      .q-btn__wrapper {
+        padding: 0;
+      }
+      .details {
+        flex: 1;
+      }
+      .name {
+          width: 100%;
+          max-width: 300px;
+          padding: 0 15px 0 0;
+          display: flex;
+          flex-direction: column;
+      }
     }
-}
 
 </style>
