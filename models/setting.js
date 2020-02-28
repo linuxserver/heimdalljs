55d8869d420b503672cfb47a9505c5a164d98aff
alex-phillips <ahp118@gmail.com>
2020-02-28 09:23:21 -0500

    settings can be a standalone value or a data structure. try and detect and set / get accordingly

--- models/setting.js
+++ models/setting.js
@@ -6,7 +6,21 @@ class Setting extends Model {
   static init (sequelize, DataTypes) {
     return super.init({
       key: DataTypes.STRING,
-      value: DataTypes.STRING
+      value: {
+        type: DataTypes.STRING,
+        get () {
+          const val = this.getDataValue('value')
+          try {
+            const retval = JSON.parse(val)
+            return retval
+          } catch (e) {
+            return val
+          }
+        },
+        set (val) {
+          this.setDataValue('value', typeof val === 'object' ? JSON.stringify(val) : val)
+        }
+      }
     }, {
       sequelize,
       underscored: true
