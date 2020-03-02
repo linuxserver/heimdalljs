be236e7a6db157394779e44644e2515c1e2549c6
Kode <kodestar@gmail.com>
2020-03-02 21:08:52 +0000

    Create EnhancedApps.js

---
+++ src/plugins/EnhancedApps.js
@@ -0,0 +1,19 @@
+export default class EnhancedApp {
+  constructor (data) {
+    this.data = data
+  }
+
+  call () {
+
+  }
+
+  fields () {
+    if (this.data.type === 1) {
+      return [
+        {
+          
+        }
+      ]
+    }
+  }
+} 
