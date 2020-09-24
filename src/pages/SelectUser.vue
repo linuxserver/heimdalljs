41124bc2541460c88bb96f13cf6f35dbfb845370
atcwells <atcwells@gmail.com>
2020-09-24 11:35:32 +0100

    Add password field, split login types to different divs

--- src/pages/SelectUser.vue
+++ src/pages/SelectUser.vue
@@ -16,30 +16,39 @@
               </q-avatar>
             </div>
             <div class="sign-in-container">
-              <q-select v-if="show_usernames" outlined v-model="selecteduser" :options="users" :label="this.$t('select_user')" color="teal" clearable option-value="id" option-label="username">
-                <template v-slot:option="scope">
-                  <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
-                    <q-item-section avatar>
-                      <q-avatar size="24px">
-                        <img class="selectuseravatar" :src="getIcon(scope.opt.avatar)" />
-                      </q-avatar>
-                    </q-item-section>
-                    <q-item-section>
-                      <q-item-label v-html="scope.opt.username" />
-                    </q-item-section>
-                  </q-item>
-                </template>
-              </q-select>
-              <q-input v-else v-model="username" :label="this.$t('username')" outlined></q-input>
+              <div v-if="show_usernames">
+                <q-select outlined v-model="selecteduser" :options="users" :label="this.$t('select_user')" color="teal" clearable option-value="id" option-label="username">
+                  <template v-slot:option="scope">
+                    <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+                      <q-item-section avatar>
+                        <q-avatar size="24px">
+                          <img class="selectuseravatar" :src="getIcon(scope.opt.avatar)" />
+                        </q-avatar>
+                      </q-item-section>
+                      <q-item-section>
+                        <q-item-label v-html="scope.opt.username" />
+                      </q-item-section>
+                    </q-item>
+                  </template>
+                </q-select>
+                <q-input v-if="hasPassword" v-model="password" :label="this.$t('password')" outlined :type="isPwd ? 'password' : 'text'">
+                  <template v-slot:append>
+                    <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+                  </template>
+                </q-input>
+              </div>
+              <div v-else>
+                <q-input v-model="username" :label="this.$t('username')" outlined></q-input>
+                <q-input v-model="password" :label="this.$t('password')" outlined :type="isPwd ? 'password' : 'text'">
+                  <template v-slot:append>
+                    <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+                  </template>
+                </q-input>
+              </div>
               <div class="public-option" v-if="hasPublic">
                 <q-btn @click="selectUser" unelevated color="cyan-5" style="width: 50%" class="">View public page</q-btn>
                 <span class="or"><span>or</span></span>
               </div>
-              <q-input v-if="hasPassword" v-model="password" :label="this.$t('password')" outlined :type="isPwd ? 'password' : 'text'">
-                <template v-slot:append>
-                  <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
-                </template>
-              </q-input>
               <q-input v-if="loginStatus === 'multifactor'" v-model="totp" :label="this.$t('totp')" outlined></q-input>
               <q-btn type="submit" unelevated color="cyan-8" style="padding: 10px" class="full-width">Login</q-btn>
             </div>
@@ -107,6 +116,9 @@ export default {
       }
       return '/heimdall-logo-white.svg'
     },
+    getUser() {
+      this.$store.dispatch('app/getUser', this.username)
+    },
     selectUser() {
       this.$store.dispatch('app/setUser', this.selecteduser)
     },
@@ -139,10 +151,12 @@ export default {
   display: flex;
   margin-bottom: 25px;
 }
+
 .avatar-container {
   padding: 30px;
   background: radial-gradient(ellipse at center, #47918a 0%, #0b3161 100%);
 }
+
 .avatar {
   display: block;
   height: 140px !important;
@@ -153,13 +167,16 @@ export default {
   width: 100%;
   max-width: 400px;
 }
+
 .sign-in-container {
   padding: 20px;
   background: #f1f1f1;
 }
+
 .q-field--outlined {
   margin-bottom: 20px;
 }
+
 .or {
   display: flex;
   padding: 10px 0;
@@ -197,6 +214,7 @@ export default {
     z-index: 1;
   }
 }
+
 .selectuseravatar {
   padding: 3px;
   background: #724c7a;
