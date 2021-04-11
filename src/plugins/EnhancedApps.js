ac1ff364e7be9197c37ecdab629af59af86f5f0a
Kode <kodestar@gmail.com>
2021-04-11 15:47:54 +0100

    fix for 0 bytes

--- src/plugins/EnhancedApps.js
+++ src/plugins/EnhancedApps.js
@@ -62,7 +62,7 @@ export default class EnhancedApps {
   }
 
   humanFileSize(size) {
-    const i = Math.floor(Math.log(size) / Math.log(1024))
+    const i = size === 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024))
     return (size / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i]
   }
 
